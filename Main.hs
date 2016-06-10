a0083075df79217e0c571fa63a3a8b6db0d3db03
Fillipe de Menezes <fmcs3@cin.ufpe.br>
2016-06-10 17:23:55 -0300

    Variaveis Locais e Globais implementadas.

--- Main.hs
+++ Main.hs
@@ -2,16 +2,21 @@ import qualified Language.ECMAScript3.Parser as Parser
 import Language.ECMAScript3.Syntax
 import Control.Monad
 import Control.Applicative
-import Data.Map as Map (Map, insert, lookup, union, toList, empty)
+import Data.Map as Map (Map, insert, lookup, union, toList, empty, intersection, difference, filter)
 import Debug.Trace
 import Value
+import Kind
 
 --
 -- Evaluate functions
 --
 
 evalExpr :: StateT -> Expression -> StateTransformer Value
-evalExpr env (VarRef (Id id)) = stateLookup env id
+evalExpr env (VarRef (Id id)) = do
+    val <-  stateLookup env id
+    case val of
+        Nil -> error $ "Variable " ++ show id ++ " not defiend." -- Cuidando do caso em que a variavel não foi definida previamente
+        _ -> return val
 evalExpr env (IntLit int) = return $ Int int
 evalExpr env (BoolLit bool) = return $ Bool bool
 evalExpr env (InfixExpr op expr1 expr2) = do
@@ -19,9 +24,13 @@ evalExpr env (InfixExpr op expr1 expr2) = do
     v2 <- evalExpr env expr2
     infixOp env op v1 v2
 evalExpr env (AssignExpr OpAssign (LVar var) expr) = do
-    stateLookup env var -- crashes if the variable doesn't exist
-    e <- evalExpr env expr
-    setVar var e
+    e <- stateLookup env var
+    val <- evalExpr env expr
+    case e of
+        Nil ->
+            setVar var (val, Global) -- Adicionamos nova variavel como Local
+        _ ->
+            setValue var val -- Nesse Caso apenas mudamos o valor da variavel e não o seu tipo
 
 evalStmt :: StateT -> Statement -> StateTransformer Value
 evalStmt env EmptyStmt = return Nil
@@ -38,13 +47,30 @@ evalStmt env (IfStmt expr ifStmt elseStmt) = do
                        else 
                             evalStmt env elseStmt
         Nil -> return val
-evalStmt env (BlockStmt stmts) = evaluate env stmts
+evalStmt env (BlockStmt stmts) = ST $ \s -> -- Cada novo blockStmt criamos um novo "conjunto de variaveis"
+    let (ST a) = return Nil
+        (t, newS) = a s
+        (ST g) = do
+            evaluate env stmts -- forStmt :v
+        (resp,ign) = g newS
+        fEnv = update ign s
+        in (resp,fEnv)
 
+
+-- Atualiza estado, dependendo das novos valores de variaveis
+-- e novas variaveis globais
+update :: StateT -> StateT -> StateT
+update new old = newGlobal
+               where disjuction = Map.difference new old
+                     upda  = Map.intersection new old
+                     oldGlobal = Map.filter (\(val,kind) -> if kind == Global then True else False) disjuction
+                     newGlobal = Map.union oldGlobal upda
 
 -- Do not touch this one :)
 evaluate :: StateT -> [Statement] -> StateTransformer Value
 evaluate env [] = return Nil
 evaluate env stmts = foldl1 (>>) $ map (evalStmt env) stmts
+
 
 --
 -- Operators
@@ -70,32 +96,40 @@ infixOp env OpLOr  (Bool v1) (Bool v2) = return $ Bool $ v1 || v2
 -- Environment and auxiliary functions
 --
 
-environment :: Map String Value
+environment :: Map String (Value,Kind)
 environment = Map.empty
 
 stateLookup :: StateT -> String -> StateTransformer Value
 stateLookup env var = ST $ \s ->
     -- this way the error won't be skipped by lazy evaluation
     case Map.lookup var (union s env) of
-        Nothing -> error $ "Variable " ++ show var ++ " not defiend."
-        Just val -> (val, s)
+        Nothing -> (Nil,s)
+        Just (val,kind) -> (val, s)
 
 varDecl :: StateT -> VarDecl -> StateTransformer Value
 varDecl env (VarDecl (Id id) maybeExpr) = do
     case maybeExpr of
-        Nothing -> setVar id Nil
+        Nothing -> setVar id (Nil,Nulo)
         (Just expr) -> do
             val <- evalExpr env expr
-            setVar id val
+            setVar id (val, Local)
 
-setVar :: String -> Value -> StateTransformer Value
-setVar var val = ST $ \s -> (val, insert var val s)
+setVar :: String -> (Value,Kind) -> StateTransformer Value
+setVar var (val,kind) = ST $ \s -> (val, insert var (val, kind) s)
 
+setValue :: String -> Value -> StateTransformer Value
+setValue var val = ST $ \s -> (val, insert var (val, helper s var) s)
+
+helper :: StateT -> String -> Kind
+helper env var = case Map.lookup var env of
+                    Nothing -> Nulo
+                    Just (value,tipo) -> tipo
+                       
 --
 -- Types and boilerplate
 --
 
-type StateT = Map String Value
+type StateT = Map String (Value,Kind)
 data StateTransformer t = ST (StateT -> (t, StateT))
 
 instance Monad StateTransformer where
